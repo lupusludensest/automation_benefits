e949f8be0eb615e9416b35ac21fe86e2adcfe3e1
LupusLudensEst <gurovvic@gmail.com>
2022-07-11 09:29:15 -0400

    dt 11 july 2022

--- automationBenefits.py
+++ automationBenefits.py
@@ -1,16 +1,4 @@
 # # Automation benefits
-# n = int(input('Enter the builds number: ')) # 10, 20, 30, 40
-# t_m = 30 #int(input('Enter the time for build testing manual: ')) # 30
-# t_a = 5 # int(input('Enter the time for build testing automation: ')) # 5
-# t_a_d = 300 # int(input('Enter the time for project automation development: ')) # 300
-# tm_fr_mnl = n * t_m
-# tm_fr_autmtn = n * t_a + t_a_d
-# effect = tm_fr_autmtn - tm_fr_mnl
-# if tm_fr_mnl >= tm_fr_autmtn:
-#     print(f'Time to start automation! Effect is : "{effect}" hours') # 12 builds is the treshhold
-# else:
-#     print(f'Manual is still efficient')
-# print(f'Time for manual: "{tm_fr_mnl}" hours\nTime for automation: "{tm_fr_autmtn}" hours')
 
 t_m = int(input('Enter the time for build testing manual: '))  # 30
 t_a = int(input('Enter the time for build testing automation: '))  # 5 five time quicker than manually
