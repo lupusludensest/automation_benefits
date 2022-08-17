918c6ac89c18be62f19ea551ed4097fc61e7149d
LupusLudensEst <gurovvic@gmail.com>
2022-08-17 17:36:26 -0500

    dt 17 aug 2022

--- automationBenefits.py
+++ automationBenefits.py
@@ -3,7 +3,7 @@
 t_m = int(input('Enter the time for build testing manual: '))  # 30
 t_a = int(input('Enter the time for build testing automation: '))  # 5 five time quicker than manually
 t_a_d = int(input('Enter the time for project automation development: '))  # 300
-number_of_builds = int(input('Enter the builds number: '))  # 12 builds is the treshhold
+number_of_builds = int(input('Enter the builds number: ')) # 12 builds is the threshold
 
 def is_aut_worth(t_m, t_a, t_a_d, number_of_builds):
     tm_fr_mnl = number_of_builds * t_m
